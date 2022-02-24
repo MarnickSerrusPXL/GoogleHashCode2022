d9a3c6227b9d615cbec459605e0d43dab54cb0f4
MarnickSerrusPXL <56915240+MarnickSerrusPXL@users.noreply.github.com>
2022-02-24 18:28:43 +0100

    added file output

--- src/puzzle.java
+++ src/puzzle.java
@@ -1,5 +1,14 @@
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class puzzle {
     public static void main(String[] args) {
-        System.out.println("hello world");
+        try {
+            FileWriter outputWriter = new FileWriter("output.txt");
+            outputWriter.write("solution");
+            outputWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
